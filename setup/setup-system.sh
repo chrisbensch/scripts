b77b1d85379eb98310c4d7cbf76d78b0493cec76
Chris Bensch <chris.bensch@gmail.com>
2023-02-17 21:08:35 +0000

    Add GitHub CLI tool

--- setup/setup-system.sh
+++ setup/setup-system.sh
@@ -398,6 +398,16 @@ git clone https://github.com/GhostPack/Rubeus.git /opt/rubeus \
 git clone https://github.com/leoloobeek/LAPSToolkit.git /opt/lapstoolkit \
   || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
 
+
+##### Install GitHub CLI Tool gh
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}GitHub CLI${RESET} ~ gh for cli "
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y \
+  || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
 ################################################################################
 
 ##### Clean the system
